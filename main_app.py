1da8d928067057c9c4b364c5f8a6933574850549
PavelFryblik <pavel.fryblik@gmail.com>
2022-07-14 20:14:55 +0200

    dva grafy

--- main_app.py
+++ main_app.py
@@ -12,7 +12,25 @@ import plotly.express as px
 
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
 
-df_bikes = pd.read_sql(sql='select * from edinburgh_bikes limit 20000', con=engine)
+query_morning = """SELECT
+               start_station_latitude as lat,
+               start_station_longitude as lon
+            FROM edinburgh_bikes
+            WHERE hour(started_at) BETWEEN 6 AND 9
+            LIMIT 100000
+        """
+
+query_afternoon = """SELECT
+               start_station_latitude as lat,
+               start_station_longitude as lon
+            FROM edinburgh_bikes
+            WHERE hour(started_at) BETWEEN 15 AND 19
+            LIMIT 100000
+        """
+
+df_bikes_morning   = pd.read_sql(sql=query_morning,   con=engine)
+df_bikes_afternoon = pd.read_sql(sql=query_afternoon, con=engine)
+
 
 # ##################
 # VIZUALICE
@@ -22,11 +40,11 @@ st.title('Moje prvni appka')
 page = st.sidebar.radio('Select page', ['Mapa','Thomson'])
 
 if page == 'Mapa':
-    st.write('Mapa pouzivani sdilenych kol v Edinburgu')
-    fig = px.scatter_mapbox(df_bikes,lat='start_station_latitude', lon='start_station_longitude')
-    fig.update_layout(mapbox_style="open-street-map")
-    fig.show()
-
+    st.header('Mapa pouzivani sdilenych kol v Edinburgu')
+    st.write('Pocatecni stanice rano mezi 6 a 9')
+    st.map(df_bikes_morning)
+    st.write('Pocatecni stanice odpoledne mezi 15 a 19')
+    st.map(df_bikes_afternoon)
 
 if page == 'Thomson':
     st.write('Thomson sampling')
